c9cc498df599abe4a40efc2bf291efe0af246406
Ariba S <153156108+asiddiqi16@users.noreply.github.com>
2024-06-24 10:23:05 +1000

    add JS for local time and fixed bug related to updateCity

--- src/index.js
+++ src/index.js
@@ -1,47 +1,51 @@
+function updateCityDateTime(cityDateTime) {
+  let firstCity = document.querySelector("#first-city");
+
+  let firstCityDate = firstCity.querySelector(".country-date");
+
+  let firstCityTime = firstCity.querySelector(".country-time");
+  firstCityDate.innerHTML = cityDateTime.substring(0, cityDateTime.length - 26);
+  firstCityTime.innerHTML = cityDateTime.substring(
+    cityDateTime.length - 26,
+    cityDateTime.length
+  );
+}
 function updateTime() {
   let selectedCity = document.getElementById("countries");
-
   if (selectedCity.value != "") {
-    let firstCity = document.querySelector("#first-city");
-
-    let firstCityDate = firstCity.querySelector(".country-date");
-
-    let firstCityTime = firstCity.querySelector(".country-time");
-
     let cityDateTime = moment()
-      .tz(selectedCity.value)
+      .tz(cityName)
       .format("dddd, MMMM Do, YYYY h:mm:ss [<small>]A[</small>]");
-
-    firstCityDate.innerHTML = cityDateTime.substring(
-      0,
-      cityDateTime.length - 25
-    );
-    firstCityTime.innerHTML = cityDateTime.substring(
-      cityDateTime.length - 25,
-      cityDateTime.length
-    );
+    updateCityDateTime(cityDateTime);
+  } else {
+    let cityDateTime = moment()
+      .tz(moment.tz.guess())
+      .format("dddd, MMMM Do, YYYY h:mm:ss [<small>]A[</small>]");
+    updateCityDateTime(cityDateTime);
   }
 }
 
 function updateCity(event) {
   if (event.target.value.length) {
     cityName = event.target.value;
+  } else {
+    cityName = moment.tz.guess();
+  }
 
-    cityNameFriendly = cityName.replace("_", " ").split("/")[1];
+  cityNameFriendly = cityName.replace("_", " ").split("/")[1];
 
-    let countriesDisplayedElement = document.querySelector(
-      ".countries-displayed"
-    );
-    countriesDisplayedElement.innerHTML = `<div class="country" id="first-city">
+  let countriesDisplayedElement = document.querySelector(
+    ".countries-displayed"
+  );
+  countriesDisplayedElement.innerHTML = `<div class="country" id="first-city">
           <div class="country-details">
             ${cityNameFriendly}
             <div class="country-date"></div>
           </div>
           <span class="country-time"></span>
         </div>`;
-  }
 }
+let selectedCity = document.getElementById("countries");
 setInterval(updateTime, 1000);
 let selectedCityElement = document.querySelector("#countries");
-
 selectedCityElement.addEventListener("change", updateCity);
